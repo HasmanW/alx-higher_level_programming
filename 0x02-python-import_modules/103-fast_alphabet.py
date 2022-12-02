46ae4e895ff52c81b6c5af96a2bebac24169206c
The_Tutor <hasmanwasilwaw@gmail.com>
2022-12-02 04:46:05 +0300

    task 103

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    for alphabet in range(65, 91):
-        print("{:c}".format(alphabet), end='\n' if alphabet == 90 else '')
+for alphabet in range(65, 91):
+    print("{:c}".format(alphabet), end='\n' if alphabet == 90 else '')
